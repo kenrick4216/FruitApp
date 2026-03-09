132231513646b2ddffde6f13d13c20f6575a4826
Ianos828 <kokbowen17@gmail.com>
2026-03-09 16:30:01 +0800

    Add placeholder bitmap if image fetching fails from esp cam

--- app/src/main/java/com/example/fruitapp/data/Esp32CamRepository.kt
+++ app/src/main/java/com/example/fruitapp/data/Esp32CamRepository.kt
@@ -1,11 +1,16 @@
 package com.example.fruitapp.data
 
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import androidx.core.content.ContextCompat
+import com.example.fruitapp.R
 import com.example.fruitapp.model.Image
 import com.example.fruitapp.network.Esp32CamApiService
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import androidx.core.graphics.createBitmap
 
 /**
  * Repository for fetching the image from the ESP32 CAM
@@ -18,12 +23,31 @@ interface Esp32CamRepository {
  * Network implementation of the Esp32CamRepository
  */
 class NetworkEsp32CamRepository(
-    private val esp32CamApiService: Esp32CamApiService
+    private val esp32CamApiService: Esp32CamApiService,
+    private val context: Context
 ) : Esp32CamRepository {
     override suspend fun getImage(): Image = withContext(Dispatchers.IO) {
-        val responseBody = esp32CamApiService.getImage()
-        val bytes = responseBody.bytes()
-        val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
-        Image(bitmap)
+        try {
+            val responseBody = esp32CamApiService.getImage()
+            val bytes = responseBody.bytes()
+            val bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.size)
+            Image(bitmap ?: getPlaceholderBitmap())
+        } catch (e: Exception) {
+            Image(getPlaceholderBitmap())
+        }
+    }
+
+    private fun getPlaceholderBitmap(): Bitmap {
+        val drawable = ContextCompat.getDrawable(context, R.drawable.ic_broken_image)
+            ?: throw IllegalStateException("Resource not found")
+        
+        val bitmap = createBitmap(
+            drawable.intrinsicWidth.coerceAtLeast(1),
+            drawable.intrinsicHeight.coerceAtLeast(1)
+        )
+        val canvas = Canvas(bitmap)
+        drawable.setBounds(0, 0, canvas.width, canvas.height)
+        drawable.draw(canvas)
+        return bitmap
     }
 }

357b04e06abf1b64ae4c7277bb728fd7cc82c7e3
kenrick4216 <e1122820@u.nus.edu>
2026-03-22 02:08:34 +0800

    Add files via upload

--- app/src/main/java/com/example/fruitapp/network/Esp32MeasurementApiService.kt
+++ app/src/main/java/com/example/fruitapp/network/Esp32MeasurementApiService.kt
@@ -1,6 +1,7 @@
 package com.example.fruitapp.network
 
 import com.example.fruitapp.model.Esp32Measurement
+import com.example.fruitapp.model.LidarScan
 import retrofit2.http.GET
 
 /**
@@ -9,4 +10,8 @@ import retrofit2.http.GET
 interface Esp32MeasurementApiService {
     @GET("trigger")
     suspend fun getMeasurements(): Esp32Measurement
+
+    // NEW: 2D lidar scan endpoint
+    @GET("lidar-scan")
+    suspend fun getLidarScan(): LidarScan
 }
